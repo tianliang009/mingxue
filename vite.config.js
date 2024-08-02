533470ca37b7efa94505b3dead217d87ea5bdbae
tianliang009 <tl958186779@163.com>
2024-08-02 09:32:20 +0800

    feat: add less

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from "path";
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "."),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        charset: false,
+        modifyVars: {}
+      }
+    }
+  }
 })
