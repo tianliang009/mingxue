5007ad1549bb4c4836a2dddbbe5485d710ee8e8f
tianliang009 <tl958186779@163.com>
2024-08-12 12:53:55 +0800

    feat: add time operation js

--- src/utils/setTime.js
+++ src/utils/setTime.js
@@ -1,4 +1,19 @@
-const timestampConversion = (date) => {
+export const formatDate = (dateString) => {
+    // 创建一个新的Date对象（默认使用当前时区）
+    const date = new Date(dateString);
+    
+    // 使用Date对象提供的方法获取年份，注意getMonth()返回的是0-11，所以需要加1
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 使用padStart()补齐2位数的格式
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    // 拼接年月日时分秒信息成为最终的字符串格式
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
+export const timestampConversion = (date) => {
     if(date.constructor == Array) {
         let str = '';
         for(let i=0; i<date.length; i++) {
@@ -10,6 +25,7 @@ const timestampConversion = (date) => {
         return(timeOpe(date))
     }
 }
+
 const timeOpe = (date) => {
     var year = date.getFullYear();
     var month = date.getMonth() + 1; // 注意月份是从0开始的，所以要+1
@@ -25,4 +41,3 @@ const timeOpe = (date) => {
     seconds = seconds < 10 ? '0' + seconds : seconds;
     return year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
 }
-export default timestampConversion;
