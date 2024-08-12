4e31eccf8a86495ba067b4a566bc0fa47b3995d0
tianliang009 <tl958186779@163.com>
2024-08-12 12:55:36 +0800

    refactor: persona page add supabase ope and update dom

--- src/pages/management/user/personal/index.jsx
+++ src/pages/management/user/personal/index.jsx
@@ -2,28 +2,38 @@ import React, {useMemo} from 'react';
 import { Table, Card, Input, DatePicker, Select, Button } from '@douyinfe/semi-ui';
 import { IconSearch, IconRefresh } from '@douyinfe/semi-icons';
 import { useState } from 'react';
-import timestampConversion from '../../../../utils/time';
-import tableData from '../../../../utils/temporary';
+// import timestampConversion from '../../../../utils/time';
+import { formatDate, timestampConversion } from '../../../../utils/setTime';
+// import tableData from '../../../../utils/temporary';
 import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { getUserData, addUserMoney } from '../../../../utils/userHelper';
+import { table } from '@douyinfe/semi-ui/lib/es/markdownRender/components';
 const PersonalUser = () => {
     const navigate = useNavigate();
     const [name, setName] = useState();
     const [account, setAccount] = useState();
+    const [startTime, setStartTime] = useState();
+    const [endTime, setEndTime] = useState();
+    const [status, setStatus] = useState();
+    const [accumulateMax, setAccumulateMax] = useState();
+    const [accumulateMin, setAccumulateMin] = useState();
+    const [tableData, setTableData] = useState();
     const statusList = [
-        { value: '0', label: '全部' },
+        { value: '', label: '全部' },
+        { value: '0', label: '停用' },
         { value: '1', label: '正常' },
-        { value: '2', label: '欠费' },
-        { value: '3', label: '停用' },
+        // { value: '3', label: '欠费' },
     ];
     const accumulateList = [
-        { value: '0', label: '全部' },
-        { value: '1', label: '100元以下' },
-        { value: '2', label: '100元-500元' },
-        { value: '3', label: '500元-1000元' },
-        { value: '4', label: '1000-5000元' },
-        { value: '5', label: '5000元-10000元' },
-        { value: '6', label: '1万元-5万元' },
-        { value: '7', label: '5万元以上' },
+        { value: '', label: '全部' },
+        { value: '1', label: '100元以下', maxNum: 100, minNum: 0 },
+        { value: '2', label: '100元-500元', maxNum: 500, minNum: 100},
+        { value: '3', label: '500元-1000元', maxNum: 1000, minNum: 500 },
+        { value: '4', label: '1000-5000元', maxNum: 5000 , minNum: 1000 },
+        { value: '5', label: '5000元-10000元', maxNum: 10000, minNum: 5000 },
+        { value: '6', label: '1万元-5万元', maxNum: 50000, minNum: 10000 },
+        { value: '7', label: '5万元以上', maxNum: null, minNum: 50000 },
     ]
     const balanceList = [
         { value: '0', label: '全部' },
@@ -35,10 +45,6 @@ const PersonalUser = () => {
     ]
     const columns = [
         {
-            title: '序号',
-            dataIndex: 'num',
-            width: 60
-        }, {
             title: '姓名',
             dataIndex: 'name',
         }, {
@@ -47,10 +53,18 @@ const PersonalUser = () => {
         }, {
             title: '注册日期',
             dataIndex: 'date',
-            width: 140,
+            width: 300,
+            render: (item) => {
+                // return <span>{Date.parse(item)}</span>
+                return <span>{formatDate(item)}</span>
+            }
         }, {
             title: '当前状态',
+            width: 100,
             dataIndex: 'status',
+            render: (item) => {
+                return <span style={{color:item === '0'?'red':'green'}}>{item === '0'?'停用':'正常'}</span>
+            }
         }, {
             title: '累计赠送金额（元）',
             dataIndex: 'giveNum',
@@ -72,7 +86,7 @@ const PersonalUser = () => {
             render: (item) => {
                 return <div className='tabOpe'>
                     <p onClick={() => {navigate(`/user/detail/:${item.num}`)}}>详情</p>
-                    <p>停用</p>
+                    <p onClick={() => {changeStatus(item.status)}}>{item.status === '0'?'启动':'停用'}</p>
                 </div>;
             }
         }
@@ -83,30 +97,75 @@ const PersonalUser = () => {
         }),
         []
     );
-    const searchOpe = () => {
-        console.log('search')
+    const searchOpe = async() => {
+        // console.log(accumulateMax,'---',accumulateMin)
+        let data = await getUserData(name, account, startTime, endTime, status, accumulateMin, accumulateMax)
+        setOpe(data)
     }
     const resertOpe = () => {
         console.log('resert')
     }
-    const setTime = (e) => {
-        let time = timestampConversion(e);
-        console.log(time)
+    const setRegister = async(e,dateString) => {
+        let startStr = dateString? dateString+' 00:00:00' : "0001-01-01 00:00:00";
+        let endStr = dateString? dateString+' 23:59:59' : "9999-12-31 23:59:59";
+        setEndTime(endStr)
+        setStartTime(startStr)
+        // let data = await getUserData('', '', `${dateString} 00:00:00`)
+        // setOpe(data)
     }
     const setValue = (e) => {
-        console.log(e)
+        setStatus(e)
     }
+    const changeStatus = (num) => {
+        console.log(num)
+    }
+    const getData = async() => {
+        let data = await getUserData()
+        setOpe(data)
+        // row假数据
+        // for(let i=0;i<data.length;i++) {
+        //     let temp = {
+        //         id: data[i].id,
+        //         user_account: data[i].account,
+        //         giveNum: Math.floor(Math.random() * 10000),
+        //         accumulate: Math.floor(Math.random() * 10000),
+        //         spend: Math.floor(Math.random() * 10000),
+        //         balance: Math.floor(Math.random() * 10000),
+        //         invoice: Math.floor(Math.random() * 10000)
+        //     }
+        //     addUserMoney(temp).then((res) => {
+        //         console.log(res)
+        //     })
+        // }
+    }
+    const setOpe = (data) => {
+        for(let i=0;i<data.length;i++) {
+            data[i].accumulate = data[i].user_money.accumulate
+            data[i].balance = data[i].user_money.balance
+            data[i].giveNum = data[i].user_money.giveNum
+            data[i].invoice = data[i].user_money.invoice;
+            data[i].spend = data[i].user_money.spend;
+        }
+        setTableData(data)
+    }
+    const changeAccumulate = (e) => {
+        setAccumulateMax(accumulateList[e].maxNum);
+        setAccumulateMin(accumulateList[e].minNum);
+    }
+    useEffect(() => {
+        getData()
+    }, [])
     return (
         <>
             <Card style={{marginBottom: '24px'}} className='search_box'>
                 <div className='search_chi'>
                     <Input prefix="姓名" placeholder="请输入姓名" value={name} onChange={(e) => {setName(e)}} showClear></Input>
                     <Input prefix="账号" placeholder="请输入账号" value={account} onChange={(e) => {setAccount(e)}} showClear></Input>
-                    <DatePicker onChange={(e) => setTime(e)} insetLabel="注册时间" />
+                    <DatePicker onChange={(e,dateString) => setRegister(e,dateString)} insetLabel="注册时间" />
                 </div>
                 <div className='search_chi'>
-                    <Select onChange={(e) => setValue(e)} optionList={statusList} insetLabel="当前状态" defaultValue={'0'}></Select>
-                    <Select optionList={accumulateList} insetLabel="累计充值" defaultValue={'0'}></Select>
+                    <Select onChange={(e) => setValue(e)} optionList={statusList} insetLabel="当前状态" defaultValue={''}></Select>
+                    <Select onChange={(e) => changeAccumulate(e)} optionList={accumulateList} insetLabel="累计充值" defaultValue={''}></Select>
                     <Select optionList={balanceList} insetLabel="费用余额" defaultValue={'0'}></Select>
                 </div>
                 <div className='' style={{display:'flex',justifyContent: 'end'}}>
